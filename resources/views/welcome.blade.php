0effc7282133f60d404cdc9d7728d90bd33b3523
kimleang <leangk49@gmail.com>
2024-08-07 18:40:23 +0700

    fixing database

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,5 @@
 @extends('layout.app')
 @section('content')
-<div class="container-fluid position-relative d-flex p-0">
-    <!-- Spinner Start -->
-    <div id="spinner" class="show bg-dark position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
-        <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
-            <span class="sr-only">Loading...</span>
-        </div>
-    </div>
-    <!-- Spinner End -->
-
-
     <!-- Sign In Start -->
     <div class="container-fluid">
         <div class="row h-100 align-items-center justify-content-center" style="min-height: 100vh;">
@@ -37,11 +27,11 @@
                         <a href="">Forgot Password</a>
                     </div>
                     <button type="submit" class="btn btn-primary py-3 w-100 mb-4">Sign In</button>
-                    <p class="text-center mb-0">Don't have an Account? <a href="">Sign Up</a></p>
+
                 </div>
             </div>
         </div>
     </div>
     <!-- Sign In End -->
-</div>
+
 @endsection
